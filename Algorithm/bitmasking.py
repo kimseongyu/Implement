02c22f2bdcf97665fc842d826fa5925ad8e6dec0
김선규 <ksk85628781@gmail.com>
2022-07-24 14:43:36 +0000

    bitmasking

--- Algorithm/bitmasking.py
+++ Algorithm/bitmasking.py
@@ -1,21 +1,47 @@
-def main():
+class bitmaksing:
+    def __init__(self):
+        self.bit = 0
 
-    operation = [["add", 1], ["toggle", 5], ["remove", 1]]
-    bit = 0
-    for i in operation:
-        op, n = i
-        if op == "add":
-            bit = bit | (1 << n)
-        elif op == "remove":
-            bit = bit & ~(1 << n)
-        elif op == "toggle":
-            bit = bit ^ (1 << n)
-        elif op == "full":
-            bit = (1 << 10) - 1
-        elif op == "empty":
-            bit = 0
-        elif op == "check":
-            print((bit >> n) & 1)
+    def add(self, n):
+        self.bit = self.bit | (1<<n)
 
-if __name__ == "__main__":
-    main()
+    def remove(self, n):
+        self.bit = self.bit & ~(1<<n)
+
+    def toggle(self, n):
+        self.bit = self.bit ^ (1<<n)
+
+    def full(self, n):
+        self.bit = (1<<(n+1)) - 1
+
+    def empty(self):
+        self.bit = 0
+
+    def check(self, n):
+        return (self.bit>>n) & 1
+
+    def size(self):
+        size = 0
+        set = self.bit >> 1
+        while set:
+            if set % 2 == 1:
+                size += 1
+            set = set >> 1
+        return size
+
+    def subSet(self):
+        list = []
+        set = self.bit >> 1
+        subset = self.bit >> 1
+        while subset:
+            list.append(bin(subset))
+            subset = (subset-1)&set
+        return list
+
+    # 실행될 함수의 인자는 부분집합
+    def subSetIterator(self, func):
+        set = self.bit >> 1
+        subset = self.bit >> 1
+        while subset:
+            func(subset)
+            subset = (subset-1)&set
